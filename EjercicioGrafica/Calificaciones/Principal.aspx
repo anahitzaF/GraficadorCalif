e1df8ecbe961910d386bf0c913e6df0e05ebe47b
Anahitza Figueroa <anahitza-figueroa@hotmail.com>
2019-01-19 15:20:25 -0700

    Se creo solución y se programó

---
+++ EjercicioGrafica/Calificaciones/Principal.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Principal.aspx.cs" Inherits="Principal" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>GRAFICACION DE CALIFICACIONES</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h5 class="card-title"> GRAFICACIÓN DE CALIFICACIONES</h5>
+                        <h6 class="card-subtitle text-muted">Para poder mostrar la información, primero se debe de seleccionar un arcivo en formato excel.</h6>
+                    </div>
+                    <div class="card-body">
+                        <input type="file" id="fileUpload" onchange="Upload()" />
+                        <div style="display:none" id="divDatos">
+                            <hr />
+                            <div class="form-row mt-2">
+                                <div class="col-md-6 col-sm-12">
+                                    <div class="col-md-12 text-center">
+                                        <label style="font-weight: bold">Calificaciones Por Alumno</label>
+                                    </div>
+                                    <canvas id="graficaBarras"></canvas>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <div class="col-md-12 text-center">
+                                        <label style="font-weight: bold">Promedio Por Grado</label>
+                                    </div>
+                                    <canvas id="graficaPastel"></canvas>
+                                </div>
+                            </div>
+
+                            <hr />
+
+                            <div class="form-row mt-2">
+                                <div class="col-md-4 col-sm-12">
+                                    <div class="col-md-12 text-center">
+                                        <label>Alumno con mayor calificación:</label>
+                                        <br />
+                                        <label id="txtAlumnoMayorCalificacion" style="font-weight: bold" />
+                                    </div>
+                                    <div class="col-md-12 text-center">
+                                        <label>Calificación:</label>
+                                        <label id="txtMayorCalificacion" style="font-weight: bold" />
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-sm-12">
+                                    <div class="col-md-12 text-center">
+                                        <label>Alumno con menor calificación:</label>
+                                        <br />
+                                        <label id="txtAlumnoMenorCalificacion" style="font-weight: bold" />
+                                    </div>
+                                    <div class="col-md-12 text-center">
+                                        <label>Calificación:</label>
+                                        <label id="txtMenorCalificacion" style="font-weight: bold" />
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-sm-12">
+                                    <div class="col-md-12 text-center">
+                                        <label>Promedio General:</label>
+                                        <label id="txtPromedioGeneral" style="font-weight: bold" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <!--Scripts-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+    <script src="js/Graficas.js"></script>
+
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.13.5/xlsx.full.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.13.5/jszip.js"></script>
+</body>
+</html>
---
+++ EjercicioGrafica/Calificaciones/js/Graficas.js
@@ -0,0 +1,243 @@
+﻿var myPieChart = new Chart($('#graficaPastel'),
+    {
+        type: 'pie',
+        data: data = {
+            datasets: [{
+                label: 'Promedios Por Grado',
+                data: [],
+                backgroundColor: []
+            }],
+            labels: []
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+var myBarChart = new Chart($('#graficaBarras'),
+    {
+        type: 'bar',
+        data: data = {
+            datasets: [{
+                label: 'Calificaciones',
+                data: [],
+                backgroundColor: []
+            }],
+            labels: []
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+function Upload() {
+
+    var fileUpload = document.getElementById("fileUpload");
+
+    //Valida si es un archivo de excel valido
+    var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx)$/;
+    if (regex.test(fileUpload.value.toLowerCase())) {
+        if (typeof (FileReader) != "undefined") {
+            var reader = new FileReader();
+
+            //Si es un navegador distinto a IE
+            if (reader.readAsBinaryString) {
+                reader.onload = function (e) {
+                    ProcessExcel(e.target.result);
+                };
+                reader.readAsBinaryString(fileUpload.files[0]);
+            } else {
+                //Para navegadores IE
+                reader.onload = function (e) {
+                    var data = "";
+                    var bytes = new Uint8Array(e.target.result);
+                    for (var i = 0; i < bytes.byteLength; i++) {
+                        data += String.fromCharCode(bytes[i]);
+                    }
+                    ProcessExcel(data);
+                };
+                reader.readAsArrayBuffer(fileUpload.files[0]);
+            }
+        } else {
+            $('#divDatos').hide();
+            alert("Este navegador no soporta HTML5.");
+        }
+    } else {
+        $('#divDatos').hide();
+        alert("Por Favor, selecciona solo archivos xls.");
+    }
+};
+
+function ProcessExcel(data) {
+    //Leo documento de Excel
+    var libroExcel = XLSX.read(data, {
+        type: 'binary'
+    });
+
+    //Obtengo el nombre de la primer hoja
+    var primerHoja = libroExcel.SheetNames[0];
+
+    //Leo todas las filas de la primer hoja  en un Json array
+    var excelRows = XLSX.utils.sheet_to_row_object_array(libroExcel.Sheets[primerHoja]);
+
+    BarChart(excelRows);
+    PieChart(excelRows);
+    textoPromedios(excelRows);
+
+    $('#divDatos').show();
+};
+
+//Llena la variable Json que se utilizará para llenar el datasets de la grafica de barras
+var BarChart = function (list) {
+    var chartToPrint = document.getElementById('graficaBarras').getContext('2d');
+
+    //Declaro la variable pero vacia
+    var json =
+        {
+            datas: [],
+            background: [],
+            label: []
+        }
+
+    //Lleno la variable "json" con cada item de la lista
+    list.forEach(function (item) {
+        json.datas.push(item.Calificacion);
+        json.background.push(getRandomColor());
+        json.label.push(item.Nombres);
+    });
+
+    removeData(myBarChart);
+    addData(myBarChart, json);
+};
+
+//Llena la variable Json que se utilizará para llenar el datasets de la grafica de pastel
+var PieChart = function (list) {
+
+    var chartToPrint = document.getElementById('graficaPastel').getContext('2d');
+
+    var json =
+        {
+            datas: [],
+            background: [],
+            label: []
+        }
+
+    var arrayPromedio = {};
+
+    //Lleno el arreglo de objetos key:Grado = [sumCalif, contador]
+    list.forEach(function (item) {
+        if (arrayPromedio[item.Grado] === undefined) {
+            arrayPromedio[item.Grado] = [parseFloat(item.Calificacion), 1];
+        }
+        else {
+            arrayPromedio[item.Grado] = [(parseFloat(arrayPromedio[item.Grado][0]) + parseFloat(item.Calificacion)), arrayPromedio[item.Grado][1] + 1];
+        }
+    });
+
+    //Lleno la variable json con el arreglo de objetos
+    for (var key in arrayPromedio) {
+        json.label.push('Grado: ' + key);
+        json.background.push(getRandomColor());
+        json.datas.push((arrayPromedio[key][0] / arrayPromedio[key][1]).toFixed(2));
+    }
+    
+    removeData(myPieChart);
+    addData(myPieChart, json);
+};
+
+var textoPromedios = function (list) {
+
+    var mejorCalif, menorCalif;
+    var promedioGeneral = 0;
+
+    //Obtengo el alumno con mejor y menor calificacion, ademas de la suma de las calificaciones de todos los alumnos
+    list.forEach(function (item) {
+        if (mejorCalif == null) {
+            mejorCalif = [item.Nombres + ' ' + item['Apellido Paterno'] + ' ' + item['Apellido Materno'], item.Calificacion];
+        }
+        else {
+            if (parseFloat(mejorCalif[1]) < parseFloat(item.Calificacion)) {
+                mejorCalif[0] = item.Nombres + ' ' + item['Apellido Paterno'] + ' ' + item['Apellido Materno'];
+                mejorCalif[1] = item.Calificacion;
+            }
+        }
+
+        if (menorCalif == null) {
+            menorCalif = [item.Nombres + ' ' + item['Apellido Paterno'] + ' ' + item['Apellido Materno'], item.Calificacion];
+        }
+        else {
+            if (parseFloat(menorCalif[1]) > parseFloat(item.Calificacion)) {
+                menorCalif[0] = item.Nombres + ' ' + item['Apellido Paterno'] + ' ' + item['Apellido Materno'];
+                menorCalif[1] = item.Calificacion;
+            }
+        }
+
+        promedioGeneral = promedioGeneral + parseFloat(item.Calificacion);
+    });
+
+    promedioGeneral = (promedioGeneral / list.length);
+
+    $('#txtAlumnoMayorCalificacion').html(mejorCalif[0]);
+    $('#txtMayorCalificacion').html(mejorCalif[1]);
+
+    $('#txtAlumnoMenorCalificacion').html(menorCalif[0]);
+    $('#txtMenorCalificacion').html(menorCalif[1]);
+
+    $('#txtPromedioGeneral').html(promedioGeneral.toFixed(2));
+};
+
+//Incerto los datos que contiene json dentro del chart de uno en uno
+function addData(chart, json) {
+    chart.data.datasets.forEach((dataset) => {
+        for (let x in json) {
+            for (let y in json[x]) {
+                if (x == "label") {
+                    chart.data.labels.push(json[x][y]);
+                }
+                else if (x == "datas") {
+                    dataset.data.push(json[x][y]);
+                }
+                else if (x == "background") {
+                    dataset.backgroundColor.push(json[x][y]);
+                }
+            }
+        }
+    });
+
+    chart.update();
+}
+
+//Elimino todos los datos que contiene el chart de uno en uno hasta quedar vacio 
+function removeData(chart) {
+    if (chart.data.labels.length !== 0) {
+        for (var i = 0; i < chart.data.labels.length;) {
+            chart.data.labels.pop();
+            chart.data.datasets.forEach((dataset) => {
+                dataset.data.pop();
+                dataset.backgroundColor.pop();
+            });
+            chart.update();
+        }
+    }
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
